658af6bd81caa07b5dd02f432e1fd0ed2755abbb
sebastien.fleury <contact@gardening-manager.com>
2017-09-12 15:20:00 +0200

    [mango] create payout

---
+++ lib/mangoex/api/payout.ex
@@ -0,0 +1,8 @@
+defmodule Mangoex.API.Payout do
+  import Mangoex.API.Base
+
+  def create(client_id, token, body) do
+    resp = request(:post, "/#{client_id}/payouts/bankwire", body, token)
+    decode_json(resp)
+  end
+end
