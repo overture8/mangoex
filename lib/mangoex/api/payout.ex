1d299380b658fe6081e38cb1c77bb570d9c00384
Phil McClure <phil.mcclure@gmail.com>
2017-10-05 15:09:57 +0100

    Fix for get_payout bug

--- lib/mangoex/api/payout.ex
+++ lib/mangoex/api/payout.ex
@@ -1,7 +1,7 @@
 defmodule Mangoex.API.Payout do
   import Mangoex.API.Base
 
-  def get(client_id, payout_id, token) do
+  def get(client_id, token, payout_id) do
     resp = request(:get, "/#{client_id}/payouts/#{payout_id}", token)
     decode_json(resp)
   end
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mangoex.Mixfile do
 
   def project do
     [app: :mangoex,
-     version: "0.2.1",
+     version: "0.2.2",
      description: "Elixir wrapper for the MangoPay API",
      package: package(),
      elixir: "~> 1.4",
